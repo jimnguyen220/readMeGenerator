af219c7be4cec64794d3cb7a31cb86d342de5087
Jim Nguyen <jimnguyen220@gmail.com>
2021-02-13 15:27:37 -0600

    added packages and generateMarkdown.js file

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require('./generateMarkdown');
 
+console.log()
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
+
+
+fs.writeFile('README.md', writeToFile, (err) =>
+ err ? console.log(err) : console.log('Creating README.md')
+ );

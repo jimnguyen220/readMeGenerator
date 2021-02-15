433f92d1b7230a121f4c1ab87fb5463e8378b9ef
Jim Nguyen <jimnguyen220@gmail.com>
2021-02-15 12:55:54 -0600

    created functioning app, commented out existing code to use later

--- index.js
+++ index.js
@@ -1,132 +1,161 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const fileName = require("./assets/generateMarkdown");
+const fileName = require('./assets/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+inquirer.prompt([
     {
-        type: "input",
-        name: "title",
-        message: "What is the name of your project?",
+        type:'input',
+        name:'title',
+        message: "What is the name of your project?"
     },
-    // {
-    //     type: "input",
-    //     name: "",
-    //     message: "",
-    // },
-    // {
-    //     type: "input",
-    //     name: "",
-    //     message: "",
-    // },
+    {
+        type:'input',
+        name:'github',
+        message: "What is your github username?"
+    },
+    {
+        type:'list',
+        name:'license',
+        message: "Please select any licenses you will be using",
+        choices: ["license 1", "license 2", "license 3", "license 4", "none"]
+    },
     
 ]).then((data) => {
-    writeToFile(fileName, data)
-});
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    questions.then((data) =>{
-`
+let readMe = `
 # ${data.title}
 
-## Description 
+## Github username: ${data.github}
 
-Your GitHub profile is an extremely important aspect of your public identity as a developer. A well-crafted one allows you to show off your work to other developers as well as potential employers. An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
-
-The quality of a README often differentiates a good project from a bad project. A good one takes advantage of the opportunity to explain and showcase what your application does, justify the technologies used, and even talk about some of the challenges you faced and features you hope to implement in the future. A good README helps you stand out among the large crowd of developers putting their work on GitHub.
-
-There's no one right way to structure a good README. There is one very wrong way, however, and that is to not include a README at all or to create a very anemic one. This guide outlines a few best practices. As you progress in your career, you will develop your own ideas about what makes a good README.
-
-At a minimum, your project README needs a title and a short description explaining the what, why, and how. What was your motivation? Why did you build this project? (Note: The answer is not "Because it was a homework assignment.") What problem does it solve? What did you learn? What makes your project stand out? 
-
-Lastly, if your project is deployed, include a link to the deployed application here.
-
-If you're new to Markdown, read the GitHub guide on [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).
-
-If you need an example of a good README, check out [the VSCode repository](https://github.com/microsoft/vscode).
-
-
-## Table of Contents (Optional)
-
-If your README is very long, add a table of contents to make it easy for users to find what they need.
-
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-
-
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-
-## Usage 
-
-Provide instructions and examples for use. Include screenshots as needed. 
-
-To add a screenshot, create an `` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-``
-``
-
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-## License
-
-The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
-
-
----
-
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, consider adding a heading called "Features" and listing them there.
-
-## Contributing
-
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
-
----
-
-© 2020 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+## Licenses
+    * ${data.license}
 `;
 
-fs.writeFile('./assets/generateREADME.md', writeToFile, (err) =>
- err ? console.log(err) : console.log('Creating README.md')
- );
+fs.writeFile("./assets/renderREADME.md", readMe, (err) =>
+err ? console.err(err) : console.log("Creating README.md"))
 });
-};
 
 
-// TODO: Create a function to initialize app
-function init() {
 
-}
 
-// Function call to initialize app
-init();
+// // TODO: Create an array of questions for user input
+// const questions = inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "title",
+//         message: "What is the name of your project?",
+//     },
+//     // {
+//     //     type: "input",
+//     //     name: "",
+//     //     message: "",
+//     // },
+//     // {
+//     //     type: "input",
+//     //     name: "",
+//     //     message: "",
+//     // },
+    
+// ]).then((data) => {
+//     writeToFile(fileName, data)
+// });
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     questions.then((data) =>{
+// `
+// # ${data.title}
+
+// ## Description 
+
+// This is where you would provide a description of your project.  
+
+// You can update headings, similar to using header tags in html by using a '#' - 
+// * An <h1> tag will be similar to using '#' followed by a space
+
+
+
+
+
+// ## Table of Contents
+
+// If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+// * [Installation](#installation)
+// * [Usage](#usage)
+// * [Credits](#credits)
+// * [License](#license)
+
+
+// ## Installation
+
+// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+
+// ## Usage 
+
+// Provide instructions and examples for use. Include screenshots as needed. 
+
+// To add a screenshot, create an `` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+// ``
+// ``
+
+
+// ## Credits
+
+// List your collaborators, if any, with links to their GitHub profiles.
+
+// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+// If you followed tutorials, include links to those here as well.
+
+
+// ## License
+
+// The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+
+
+// ---
+
+// 🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+// ## Badges
+
+// ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+// Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+// ## Features
+
+// If your project has a lot of features, consider adding a heading called "Features" and listing them there.
+
+// ## Contributing
+
+// If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+// ## Tests
+
+// Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+// ---
+
+// © 2020 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+// `;
+
+// fs.writeFile('./assets/generateREADME.md', writeToFile, (err) =>
+//  err ? console.log(err) : console.log('Creating README.md')
+//  );
+// });
+// };
+
+
+// // TODO: Create a function to initialize app
+// function init() {
+
+// }
+
+// // Function call to initialize app
+// init();
 
